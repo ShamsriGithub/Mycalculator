2dfebd4b75c9c7ce08a4b1cc5c6c77381bec6b30
shambhavee <shambhaveesrivastava@gmail.com>
2022-10-17 13:17:21 +0530

    my first cmmit

---
+++ src/Myintpkg/maininteclass.java
@@ -0,0 +1,13 @@
+package Myintpkg;
+
+public class maininteclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sumoofintegers oobj = new sumoofintegers();
+		System.out.println(oobj.sum());
+		
+
+	}
+
+}
---
+++ src/Myintpkg/sumoofintegers.java
@@ -0,0 +1,14 @@
+package Myintpkg;
+
+public class sumoofintegers {
+	int sum()
+	{
+		int sum=0;
+		for(int i=1;i<=250;i++)
+		{
+			sum=sum+i;
+		}
+		return sum;
+	}
+
+}

3a8574f0edd82d927160b4000c773afe1c19ccfd
shambhavee <shambhaveesrivastava@gmail.com>
2022-10-17 12:20:19 +0530

    my second commit

--- src/MyCalcpkg/mainCalc.java
+++ src/MyCalcpkg/mainCalc.java
@@ -5,8 +5,11 @@ public class mainCalc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyCalc obj = new MyCalc();
+		MyCalc srm= new MyCalc();
 		int sum=obj.addition(345,567);
+		int sub=srm.substraction(45,9);
 		System.out.println(sum);
+		System.out.println(sub);
+		
 	}
-
 }

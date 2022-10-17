6267c86b69ddd5a9ded60b13644ffac576433445
shambhavee <shambhaveesrivastava@gmail.com>
2022-10-17 11:52:08 +0530

    My first commit

---
+++ src/MyCalcpkg/mainCalc.java
@@ -0,0 +1,12 @@
+package MyCalcpkg;
+
+public class mainCalc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyCalc obj = new MyCalc();
+		int sum=obj.addition(345,567);
+		System.out.println(sum);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 91829
+ *
+ */
+module Mycalculator {
+}

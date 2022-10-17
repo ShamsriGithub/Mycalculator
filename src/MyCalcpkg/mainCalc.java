9606214318c89c665e3c4f110df017e8e439672c
shambhavee <shambhaveesrivastava@gmail.com>
2022-10-17 12:29:16 +0530

    my fourth commit

--- src/MyCalcpkg/mainCalc.java
+++ src/MyCalcpkg/mainCalc.java
@@ -7,12 +7,16 @@ public class mainCalc {
 		MyCalc obj = new MyCalc();
 		MyCalc srm= new MyCalc();
 		MyCalc mul=new MyCalc();
+		MyCalc div=new MyCalc();
 		int sum=obj.addition(345,567);
 		int sub=srm.substraction(45,9);
-		int mult=mul.multiply(23, 5);
+		int mult=mul.multiply(23,5);
+		int divi=div.divide(25,5);
 		System.out.println(sum);
 		System.out.println(sub);
 		System.out.println(mult);
+		System.out.println(divi);
+		
 		
 	}
 }
